966fc05d41e9ef8d2c0d7706acc5b408bb011f50
eoris <eoris.work@gmail.com>
2015-10-30 17:22:49 +0200

    to_a

--- factory.rb
+++ factory.rb
@@ -2,9 +2,8 @@ class Factory
   def self.new(*attributes, &block)
     class_name = attributes.shift.capitalize if attributes.first.is_a?(String)
     class_name_from_string = Class.new do
-      class_name.to_s
       attributes.each do |attribute|
-        class_eval { attr_accessor attribute }
+        attr_accessor attribute
       end
 
       define_method :initialize do |*value|
@@ -29,10 +28,51 @@ class Factory
         end
       end
 
-      define_method :each do |&attribute|
-        instance_variables.map { |i| instance_variable_get(i) }.
-        each(&attribute)
+      define_method :each do |&attribute_value|
+        values.each(&attribute_value)
       end
+
+      # define_method :each_pair do |&attribute|
+      #   joe.each_pair {|name, value| puts("#{name} => #{value}") }
+      # end
+
+      # define_method :eql? do
+      # end
+
+      # define_method :hash do
+      # end
+
+      # define_method :inspect do
+      # end
+
+      # define_method :length do
+      # end
+
+      # define_method :members do
+      # end
+
+      # define_method :select do
+      # end
+
+      # define_method :size do
+      # end
+
+      define_method :to_a do
+        instance_variables.map { |i| instance_variable_get(i) }
+      end
+      alias_method :values, :to_a
+
+      # define_method :to_h do
+      # end
+
+      # define_method :to_s do
+      # end
+
+      # define_method :values do
+      # end
+
+      # define_method :values_at do
+      # end
 
       class_eval(&block) if block_given?
     end
@@ -47,7 +87,7 @@ Customer = Factory.new(:name, :address, :zip) do
   end
 end
 
-joe = Customer.new('Joe Smith,', '123 Maple, Anytown NC', 12345)
+joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
 joe.name
 joe['name']
 joe[:name]
@@ -56,3 +96,4 @@ joe.greeting
 
 Factory.new('Customer', :name, :address, :zip)
 joe.each { |i| puts i }
+#joe.each_pair {|name, value| puts("#{name} => #{value}") }
